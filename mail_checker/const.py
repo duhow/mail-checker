0ebebea9c728ce4658fefd2dde78e8a887c68e99
duhow <contacto@duhowpi.net>
2024-10-23 21:46:37 +0200

    add more domains

--- mail_checker/const.py
+++ mail_checker/const.py
@@ -89,7 +89,11 @@ public_email_providers = [
 
 typos_gmail = [
   "com.gmail",
+  "g.mail",
+  "gmail.com.email",
+  "gmai.com",
   "gmail.om",
+  "gmail.co",
   "gmail.con",
   "gmail.coom",
   "gomail.com",
@@ -98,22 +102,37 @@ typos_gmail = [
   "gmill.com",
   "gmill.con",
   "gamil.ccom",
-  "gmail.com.ar",
+  "gmail.com.ar", # !
+  "gmail.com.br", # !
+  "gamil.com.br",
+  "gmailcom.br",
+  "gamial.com",
+  "gmilcom.com",
+  "gmael.com",
   "gimal.com",
   "gmil.com",
   "gemal.com",
   "gemil.com",
   "geml.com",
   "gmaile.com",
+  "gqmil.com",
   "gmei.com",
   "gmeli.com",
   "gmaim.com",
+  "gamail.com",
+  "gtmail.com",
   "gmamail.com",
+  "gmaol.com",
   "gmail.coml",
   "gmail.cl",
+  "imail.com",
+  "gimail.com",
   "amil.com",
   "agmail.com",
   "gamul.com",
+  "gmaip.com",
+  "gmain.com",
+  "gamil.com",
   "fmail.com",
   "jmail.com",
   "jimail.com",
@@ -125,11 +144,19 @@ typos_gmail = [
   "gmaii.com",
   "gilam.com",
   "mgail.com",
+  "gilm.com",
+  "gemeio.com",
   "gami.com",
   "gmqil.com",
+  "gamol.com",
+  "gmiaal.com",
+  "gemai.com",
+  "gamli.com",
   "gmail.co.com",
   "gmail.com.pe",
+  "gmail.l.com",
   "gmilcom.com",
+  "ymail.com", # WARNING! THIS IS YAHOO REAL, but it's a common typo
 ]
 
 typos_hotmail = [
@@ -140,6 +167,11 @@ typos_hotmail = [
   "hotmaim.com",
   "hotmaip.com",
   "hotmaul.com",
+  "hotemail.com",
+  "ghotmail.com",
+  "homail.com",
+  "hotml.com",
+  "hotmail.c.com",
   "otmai.com",
   "hotmail.con",
   "hotmail.cm",
@@ -154,7 +186,14 @@ typos_icloud = [
   "iclud.com",
   "iclou.com",
   "icoud.com",
+  "icluod.com",
   "iclound.com",
+  "i.cloud.com",
+]
+
+typos_yahoo = [
+  "uahoo.com",
+
 ]
 
 domain_typos = {
